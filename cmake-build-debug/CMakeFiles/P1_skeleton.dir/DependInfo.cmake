aac6f923a554ebbac3dc0480c6256d1ec70483ff
joeabisleiman <joe.abisleiman@gmail.com>
2017-10-14 14:19:20 -0400

    Init

---
+++ cmake-build-debug/CMakeFiles/P1_skeleton.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/Joe Abi Sleiman/Dropbox/Uni/Cornell/Courses/NDS/Assignments/Assignment 2/Skeleton Code/P1_skeleton/gbn.c" "/cygdrive/c/Users/Joe Abi Sleiman/Dropbox/Uni/Cornell/Courses/NDS/Assignments/Assignment 2/Skeleton Code/P1_skeleton/cmake-build-debug/CMakeFiles/P1_skeleton.dir/gbn.c.o"
+  "/cygdrive/c/Users/Joe Abi Sleiman/Dropbox/Uni/Cornell/Courses/NDS/Assignments/Assignment 2/Skeleton Code/P1_skeleton/receiver.c" "/cygdrive/c/Users/Joe Abi Sleiman/Dropbox/Uni/Cornell/Courses/NDS/Assignments/Assignment 2/Skeleton Code/P1_skeleton/cmake-build-debug/CMakeFiles/P1_skeleton.dir/receiver.c.o"
+  "/cygdrive/c/Users/Joe Abi Sleiman/Dropbox/Uni/Cornell/Courses/NDS/Assignments/Assignment 2/Skeleton Code/P1_skeleton/sender.c" "/cygdrive/c/Users/Joe Abi Sleiman/Dropbox/Uni/Cornell/Courses/NDS/Assignments/Assignment 2/Skeleton Code/P1_skeleton/cmake-build-debug/CMakeFiles/P1_skeleton.dir/sender.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/P1_skeleton.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1_skeleton.dir/gbn.c.o"
+  "CMakeFiles/P1_skeleton.dir/receiver.c.o"
+  "CMakeFiles/P1_skeleton.dir/sender.c.o"
+  "P1_skeleton.pdb"
+  "P1_skeleton.exe"
+  "libP1_skeleton.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/P1_skeleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
